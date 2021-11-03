b183b63a3c9d1d01a984ad498df0a2de082a33db
abhinavsiwal <abhinavsiwal@gmail.com>
2021-11-03 04:51:57 -0700

    home

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import './App.css';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Home />
      <Footer />
     </div>
   );
