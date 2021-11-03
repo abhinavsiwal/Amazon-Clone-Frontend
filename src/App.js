26470cefd5e2b431fbea56dddf7d3ca943f67d62
abhinavsiwal <abhinavsiwal@gmail.com>
2021-11-03 00:37:48 -0700

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import React from 'react';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+     <Header />
+     <Footer />
     </div>
   );
 }
